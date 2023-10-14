ab8b0b5511dd087956917ed54834bae709cd6ade
Abu Taher <writerabutaher@gmail.com>
2023-10-14 23:04:38 +0600

    job card design

--- src/components/Home/Job/Job.jsx
+++ src/components/Home/Job/Job.jsx
@@ -4,11 +4,11 @@ import JobCard from "./JobCard";
 
 const Job = () => {
   return (
-    <div className="container flex justify-center gap-4 pb-20 mx-auto">
+    <div className="container flex justify-center gap-8 pb-20 mx-auto">
       <div className="w-1/4 px-4 py-8 bg-slate-200 rounded-2xl">
         <JobList />
       </div>
-      <div className="w-3/4 p-5 bg-red-100 rounded">
+      <div className="w-3/4">
         <JobCard />
       </div>
     </div>
--- src/components/Home/Job/JobCard.jsx
+++ src/components/Home/Job/JobCard.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Banknote } from "lucide-react";
 
 const JobCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="grid grid-cols-2 gap-8">
+        {[1, 2, 3, 4, 5, 6].map((e) => (
+          <div className="space-y-4 bg-white shadow-xl rounded-xl h-[300px] p-8 hover:outline hover:outline-1 hover:outline-blue-600">
+            <div className="space-y-2">
+              <h1 className="text-2xl font-semibold">
+                Digital Marketing Manager
+              </h1>
+              <h3 className="text-lg font-medium tracking-widest">
+                $95k – $150k <span className="text-gray-500">/ Remote</span>
+              </h3>
+            </div>
+            <div className="space-y-2">
+              <div className="flex gap-2">
+                <img src="" alt="" />
+                <div className="space-y-2">
+                  <h1>
+                    Atlas Bank <Banknote size={"1.5rem"} />
+                  </h1>
+                  <h3 className="text-gray-600">San Francisco, CA</h3>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button className="w-full py-4 text-center border border-blue-600 hover:bg-blue-600 rounded-xl hover:text-white">
+                View Job | {">"}
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default JobCard;
