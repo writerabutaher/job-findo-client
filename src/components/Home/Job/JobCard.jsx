f579ff78b33d31f6739633a095336b402c687892
Abu Taher <writerabutaher@gmail.com>
2023-10-14 22:22:59 +0600

    Update JobCard.jsx

--- src/components/Home/Job/JobCard.jsx
+++ src/components/Home/Job/JobCard.jsx
@@ -1,64 +1,7 @@
 import React from "react";
 
 const JobCard = () => {
-  return (
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis unde
-      dignissimos doloremque reiciendis eveniet labore quo illum iusto quidem
-      sequi distinctio ad eligendi sapiente, voluptatibus tempora quam officiis,
-      dolor harum voluptas? Eveniet quasi, possimus excepturi commodi dolore
-      neque temporibus necessitatibus officiis cumque tempore labore quis atque
-      expedita minus! Porro quibusdam nam unde expedita ratione reiciendis
-      doloribus blanditiis labore! Veniam quam voluptatem voluptatibus quis esse
-      vitae necessitatibus eaque in quia? Aspernatur iusto accusamus doloremque,
-      maiores voluptates ducimus minima molestiae necessitatibus nesciunt at id
-      eos officiis obcaecati magni corporis enim debitis repellendus modi
-      molestias esse! Similique consectetur deleniti sapiente laborum corporis
-      dolore libero repellendus cum. Deserunt nihil iste sit, velit vitae nemo
-      odio optio qui suscipit, minus repudiandae, excepturi molestiae in
-      maiores. Tenetur ipsum veritatis labore et vero corrupti ea natus, sequi
-      temporibus quasi. Odio eum reiciendis, temporibus suscipit maiores iusto
-      illum fugit quis doloribus magnam earum, odit officiis, inventore enim!
-      Eius explicabo deserunt suscipit molestiae, autem quae doloremque, sint
-      animi qui provident voluptas blanditiis consectetur atque eum totam at
-      asperiores tempora et. Adipisci nihil et accusamus laudantium natus soluta
-      minus. Dignissimos quibusdam quasi nobis, at est amet deleniti ex
-      perspiciatis illo repellendus numquam rem quia similique autem modi nulla
-      eligendi excepturi corrupti, officiis ratione. Fuga enim libero
-      repellendus autem, animi dolores eum quam natus nobis doloribus quisquam
-      quae tempore, soluta reprehenderit voluptate facere unde explicabo qui
-      dolorem? Eum ratione quas maxime vitae animi iusto, mollitia, incidunt
-      quidem, error repellat eligendi quae provident ad illo iste. Quidem,
-      numquam esse. Cumque consectetur, quisquam itaque maiores quos error
-      commodi voluptates rerum, laboriosam laudantium natus impedit reiciendis
-      aspernatur. Incidunt nesciunt repellat nostrum porro obcaecati hic nam
-      vero corporis beatae, laborum molestias reprehenderit, labore totam earum
-      delectus numquam explicabo qui perspiciatis esse eveniet minus eum fugit
-      praesentium suscipit. Error sapiente temporibus delectus nostrum pariatur
-      quibusdam officia sequi. Impedit, delectus saepe. Veritatis vitae
-      laboriosam alias doloremque odio ea corporis reiciendis, tempore quibusdam
-      ipsum architecto repudiandae laudantium! Ipsam corporis quisquam suscipit
-      dignissimos ea cum ad excepturi esse nihil, sint ratione rerum
-      necessitatibus maxime unde quaerat fugit praesentium illo ex maiores
-      repudiandae. Autem laudantium, corporis ad illum repellendus possimus
-      reprehenderit nihil earum quidem excepturi fugiat recusandae dolor veniam
-      error impedit veritatis doloremque neque, dolore at ipsa accusamus eius.
-      Sit ipsum quaerat dolor labore debitis obcaecati velit illo quis ea eum
-      quidem ex libero, soluta pariatur, eius maiores ut voluptates accusantium
-      repellendus unde laudantium. Asperiores nulla quia a voluptatum officia
-      alias quod, quasi, suscipit harum accusamus temporibus unde nihil qui ad
-      atque perferendis odio! Mollitia eligendi praesentium accusantium a ipsum
-      dignissimos. Corrupti consequuntur nam, delectus nobis vero est ab
-      deleniti iste et animi nihil libero alias incidunt eum pariatur esse
-      molestiae accusamus! Magnam, quidem vitae animi eum, placeat dolore, ullam
-      tempore eveniet expedita distinctio ratione tenetur aspernatur laboriosam
-      voluptate adipisci magni nisi vel repudiandae. Officiis dicta odio illo
-      nesciunt a accusantium temporibus officia, quibusdam sit voluptatem ipsa.
-      Nulla repellendus vel ad deserunt. Ullam repudiandae labore culpa.
-      Repellat qui, ipsam esse totam nemo provident iusto dolorum maxime
-      sapiente cupiditate ex quas?
-    </div>
-  );
+  return <div></div>;
 };
 
 export default JobCard;
